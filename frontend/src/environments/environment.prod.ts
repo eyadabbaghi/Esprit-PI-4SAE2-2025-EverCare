ec708642f3dd312db8751ba553a5045a71fda48f
Badr Klila <klilabader2@gmail.com>
2026-05-06 23:50:16 +0100

    Add aiServiceUrl to production environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,4 +4,5 @@ export const environment = {
   authApiUrl: 'http://keycloak:8080',
   medicalRecordApiUrl: 'http://medical-record-service:8083/api/medical-records',
   cognitiveStimulationApiUrl: 'http://cognitive-stimulation-service:8084/api',
+  aiServiceUrl: 'http://evercare-ai-service:8000'
 };
